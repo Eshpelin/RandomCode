c9f85fb87d8aa1b16acd6f4bea37fd1619eee70b
Eshpelin <eshpelin@gmail.com>
2017-10-10 03:00:57 +0600

    Testing the Java subSequence Method

---
+++ CodeTestingPlayground/subsequenceTester.java
@@ -0,0 +1,7 @@
+public class subsequenceTester {
+ public static void main(String[] args) {
+  String myString = "Say Dick is Saadat is a Dick";
+  System.out.println(myString.subSequence(myString.length() - 4, myString.length()));
+ }
+
+}
