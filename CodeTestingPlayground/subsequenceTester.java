60ad62fd249e00f676633f9840bf828b6c1ac0de
Eshpelin <eshpelin@gmail.com>
2017-10-15 23:54:04 +0600

    Small Change

--- CodeTestingPlayground/subsequenceTester.java
+++ CodeTestingPlayground/subsequenceTester.java
@@ -1,6 +1,6 @@
 public class subsequenceTester {
  public static void main(String[] args) {
-  String myString = "Say Dick is Saadat is a Dick";
+  String myString = "Say Dick if Saadat is a Dick";
   System.out.println(myString.subSequence(myString.length() - 4, myString.length()));
  }
 
